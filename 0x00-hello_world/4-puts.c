afaf93c9e5e199411378d8b79616a99eb719e7a6
Otanga-Otieno <otieno.otanga@gmail.com>
2022-06-30 05:55:21 -0700

    Comply with betty documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+/* Print one line using puts */
 int main(void)
 {
-	char str[52];
+	char str[54];
 
-	strcpy(str, "Programming is like building a multilingual puzzle\n");
+	strcpy(str, "\"Programming is like building a multilingual puzzle\n\"");
 	puts(str);
 	return (0);
 }
