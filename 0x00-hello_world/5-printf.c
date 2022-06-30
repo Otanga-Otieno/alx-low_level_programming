084bc0edc9774605572fdc24e175ba3a35970bf7
Otanga-Otieno <otieno.otanga@gmail.com>
2022-06-30 06:18:06 -0700

    Print line with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - Print line with printf
+ *Return: 0 if function succeeds
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
