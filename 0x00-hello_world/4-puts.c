11b4829972e138a4091d5aef00750a184ef4c83b
Otanga-Otieno <otieno.otanga@gmail.com>
2022-06-30 06:10:12 -0700

    Remove new line after output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	char str[53];
+	char str[52];
 
-	strcpy(str, "\"Programming is like building a multilingual puzzle\n");
+	strcpy(str, "\"Programming is like building a multilingual puzzle");
 	puts(str);
 	return (0);
 }
