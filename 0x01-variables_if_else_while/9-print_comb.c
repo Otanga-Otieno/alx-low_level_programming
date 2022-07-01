2b0f677b1c50789c64c3bb68d8c8b88c655bb3fb
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-01 14:19:43 -0700

    Print single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print single digits
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
