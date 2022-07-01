9a084a46cb55dea2c339a5cfc7013c20490ba8f3
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-01 13:39:20 -0700

    Print numbers using putchar

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet letters
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers 0 to 9;
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
