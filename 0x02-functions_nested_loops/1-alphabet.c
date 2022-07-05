fd597ccbb4b1f28547be76ea8bee92240473a83b
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-05 07:36:04 -0700

    Print letters of the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Print alphabet letters
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = "a"; c <= "z"; c++)
+	{
+		_putchar(c);
+	}
+	_putchar("\n");
+	return (0);
+}
