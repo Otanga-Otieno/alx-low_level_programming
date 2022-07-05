0f814dc293cc70aab7705ec7ddb59a7b2d7f5422
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-05 11:23:03 -0700

    Print absolute value of n

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Return absolute value
+ * Return: value of int
+ * @n: n is an integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
