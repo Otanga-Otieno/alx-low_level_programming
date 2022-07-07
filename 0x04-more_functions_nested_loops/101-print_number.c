8d735118d04237f3d5edf4d13e65c28f76c091f2
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-07 10:39:31 -0700

    Print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_number - Print an integer
+ * @n: n is an integer
+ */
+
+void print_number(int n)
+{
+	unsigned int tens, digit, positive = n;
+	double d = 1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		if (n < 0)
+		{
+			positive = n * -1;
+			_putchar('-');
+		}
+
+		while (d <= positive)
+		{
+			t_beg *= 10;
+		}
+		tens = d / 10;
+
+		while (tens >= 1)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
+	}
+}
