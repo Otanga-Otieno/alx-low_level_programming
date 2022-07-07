41f052b38918f2a9193be50648f508265ff489a9
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-07 06:58:02 -0700

    Print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print a triangle
+ *@size: size is an integer
+ */
+
+void print_triangle(int size)
+{
+	int count, width, height;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (height = 1; height <= size; height++)
+	{
+		for (width = 1; width <= (size - height); width++)
+			_putchar(' ');
+
+		for (draw = 1; draw <= height; draw++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
