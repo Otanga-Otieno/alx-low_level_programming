cf1d4bf9321140cdb96b995397bfa61efbd31bcd
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-11 20:00:13 +0300

    Print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strlen - Get string length
+ * @s: string
+ * Return: integer, string length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * puts2 - Print every other character of string
+ * @str: a string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
