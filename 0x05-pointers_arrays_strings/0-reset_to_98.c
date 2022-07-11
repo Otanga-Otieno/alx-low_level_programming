0f25ba5cb529904e900c0d53a85b0d25e8ce2176
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-11 02:12:37 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Set value to 98
+ * @n: n is a pointer to an integer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Tasks on pointers and arrays
