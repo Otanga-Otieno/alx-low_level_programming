0748896be29d040fa816f1407f1442ece96a900b
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-11 18:10:51 +0300

    Add function

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - Return length of a string
+ * Return: integer, length of string
+ * @s: a string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
  * rev_string - Reverse a string
  * @s: a string
  */
