aff233e931003e0b6a455c00f9ef2cdd6974090c
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-14 23:03:20 +0300

    Add tasks

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_number - Print integer
+ * @n: integer
+ */
+
+void print_number(int n)
+{
+	unsigned int tens, digit;
+	unsigned int positive = n;
+	double d = 1;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			positive = n * -1;
+			_putchar('-');
+		}
+
+		while (d <= positive)
+			d *= 10;
+		tens = d / 10;
+
+		while (tens >= 1)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
+	}
+}
