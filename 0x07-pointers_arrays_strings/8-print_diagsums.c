3eaab54f90501733cb1691fd928400908e3e05b9
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-19 00:10:29 +0300

    Betty

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,13 +9,14 @@
 
 void print_diagsums(int *a, int size)
 {
-    int i;
-    int diag1 = 0;
+	int i;
+	int diag1 = 0;
 	int diag2 = 0;
 
-    for (i = 0; i < size; i++) {
+	for (i = 0; i < size; i++)
+	{
 		diag1 += a[(i * size) + i];
 		diag2 += a[(i * size + size) - i - 1];
-    }
+	}
 	printf("%d, %d\n", diag1, diag2);
 }
