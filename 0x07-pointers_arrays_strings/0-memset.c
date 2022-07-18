4de9cd567173677610af8fd323e31eff9ab82834
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-18 16:50:51 +0300

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memset - Fill memory with bytes
+ * Return:  pointer to the memory area @s
+ * @s: a memory area
+ * @b: character to use as constant byte
+ * @n: number of bytes to fill
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int count = 0;
+
+	while (count < n)
+	{
+		*s = b;
+		s++;
+		n++;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointers to pointers, multi dimensional arrays tasks
