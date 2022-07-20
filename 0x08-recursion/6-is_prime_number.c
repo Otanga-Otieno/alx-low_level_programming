d8f922b66f766540eeb658d2b1af777e6e430c6c
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-20 14:40:04 +0300

    Check prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * prime - check primeness of a number 
+ * return - 1 if prime otherwise 0
+ */
+
+int prime(int num, int divisor)
+{
+	if (divisor >= num)
+	{
+		return (1);
+	}
+	if ((num % divisor) != 0)
+	{
+		return prime(num, divisor + 1);
+	}
+	return (0);
+}
+
+/**
+ * is_prime_number - Find if number is a prime
+ * Return: 1 if prime otherwise 0
+ * @n: integer
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 1 || n == -1)
+	{
+		return (0);
+	}
+	return prime(n, 2);
+
+}
