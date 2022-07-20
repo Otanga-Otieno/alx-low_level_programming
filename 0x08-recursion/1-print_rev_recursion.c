9ef0e1cf69c4fe5475f6883d4dd7261eb341c9a7
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-20 12:49:47 +0300

    Print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Print string in reverse
+ * s - pointer to string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(++s);
+	_putchar(*s);
+
+}
