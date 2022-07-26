593846851d11e31016a8ae2d945c5fd71d8c2f5f
Otanga-Otieno <otieno.otanga@gmail.com>
2022-07-26 22:45:56 +0300

    Free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free 2d grid
+ * @grid: 2d array to free
+ * @height: height of array
+ */
+
+void free_grid(int **grid, int height)
+{
+	free(grid);
+}
